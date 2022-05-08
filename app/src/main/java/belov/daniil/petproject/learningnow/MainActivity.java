6f36266c8db9ec081c46de46824e79111abb0c49
JoreKut <37114216+JoreKut@users.noreply.github.com>
2022-05-08 22:13:00 +0300

    made main activity by design

--- app/src/main/java/belov/daniil/petproject/learningnow/MainActivity.java
+++ app/src/main/java/belov/daniil/petproject/learningnow/MainActivity.java
@@ -2,6 +2,7 @@ package belov.daniil.petproject.learningnow;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ActionBar;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
